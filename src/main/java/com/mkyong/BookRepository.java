5d874636740b488fee145d5beea527bcf75b52dd
Jaydeep <jaydeep.sankpal@gmail.com>
2019-12-29 20:14:25 +0530

    added working code base

---
+++ src/main/java/com/mkyong/BookRepository.java
@@ -0,0 +1,7 @@
+package com.mkyong;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Spring Data magic :)
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
